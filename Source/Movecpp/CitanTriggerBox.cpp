547402ad80c222be3345b7c1b4586afa484952c5
Max Santos <maxedwardsantos@gmail.com>
2021-10-23 20:26:29 -0400

    Added Trigger Area for NPC Dialogue Prompt

---
+++ Source/Movecpp/CitanTriggerBox.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CitanTriggerBox.h"
+#include "DrawDebugHelpers.h"
+
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, text)
+#define printf(text, fstring) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT(text), fstring))
+
+// Default Values (defined in header file as constructor)
+ACitanTriggerBox::ACitanTriggerBox()
+{
+	// Register Events
+	OnActorBeginOverlap.AddDynamic(this, &ACitanTriggerBox::OnOverlapBegin);
+	OnActorEndOverlap.AddDynamic(this, &ACitanTriggerBox::OnOverlapEnd);
+}
+
+void ACitanTriggerBox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Purple, true, -1, 0, 5);
+}
+
+void ACitanTriggerBox::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	// If other actor is overlapping (excluding itself)
+	if (OtherActor && (OtherActor != this))
+	{
+		print("Overlap Begin");
+		printf("Overlapped Actor = %s", *OverlappedActor->GetName());
+	}
+}
+
+void ACitanTriggerBox::OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	// If other actor is overlapping (excluding itself)
+	if (OtherActor && (OtherActor != this))
+	{
+		print("Overlap Ended");
+		printf("Overlapped Actor = %s", *OtherActor->GetName());
+	}
+}
+
---
+++ Source/Movecpp/CitanTriggerBox.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "CitanTriggerBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVECPP_API ACitanTriggerBox : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+
+protected:
+	// Called when game starts/object spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	// Constructor sets default values for this actor's properties
+	ACitanTriggerBox();
+
+	// Overlap begin function declaration
+	UFUNCTION()
+	void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
+
+	// declare overlap end function
+	UFUNCTION()
+	void OnOverlapEnd(class AActor* OverlappedActor, class AActor* OtherActor);
+};

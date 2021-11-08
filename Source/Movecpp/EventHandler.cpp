df7802f2f3d40391786957d9bfbab267f7ce87ab
Max Santos <maxedwardsantos@gmail.com>
2021-11-07 21:15:52 -0500

    Added Box Component to character to handle our events instead of using seperate trigger boxes

--- Source/Movecpp/EventHandler.cpp
+++ Source/Movecpp/EventHandler.cpp
@@ -1,15 +1,54 @@
 #include "MainCharacter.h"
 
-// Function goes here for event triggering
+// These are to help with Debugging
+#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green, text)
+#define printf(text, fstring) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT(text), fstring))
+
+// Box Collider On Overlap Begin
+void AMainCharacter::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+	eventPossible = true;
+	
+	// If other actor is overlapping (excluding itself)
+	if (OtherActor && (OtherActor != this))
+	{
+		eventPossible = true;
+		
+		printf("Overlapped Actor = %s", *OtherActor->GetName());
+
+		PromptPlayer();
+	}
+}
+// Box Collider On Overlap End
+void AMainCharacter::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	// If other actor is overlapping (excluding itself)
+	if (OtherActor && (OtherActor != this))
+	{
+		eventPossible = false;
+		
+		printf("Overlapped with %s has ended", *OtherActor->GetName());
+
+		UnpromptPlayer();
+	}
+}
+
+
+// Show Event Indicator
 void AMainCharacter::PromptPlayer()
 {
 	EventPrompt->SetVisibility(true);
 	EventPrompt->SetSpriteColor(FLinearColor(1.0, 1.0, 1.0, 1.0));
 }
-
+// Hide Event Indicator
 void AMainCharacter::UnpromptPlayer()
 {
 	EventPrompt->SetVisibility(false);
 	EventPrompt->SetSpriteColor(FLinearColor(1.0, 1.0, 1.0, 0.0));
 }
+
+
 
